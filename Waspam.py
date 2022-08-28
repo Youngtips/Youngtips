b21ccb27f200f612110a39553b080e1cb1021735
Youngtips <98290454+Youngtips@users.noreply.github.com>
2022-08-28 15:08:52 +0200

    Add files via upload

---
+++ Waspam.py
@@ -0,0 +1,11 @@
+print("≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶Freeze@@$#_#_¥£¥£¥£≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶" * 900000)
+
+print("≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶Freeze@@$#_#_¥£¥£¥£≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶" * 900000)
+
+print("≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶Freeze@@$#_#_¥£¥£¥£≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶" * 900000)
+
+
+print("≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶Freeze@@$#_#_¥£¥£¥£≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶" * 900000)
+
+
+print("≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶Freeze@@$#_#_¥£¥£¥£≪Yø͢͢͢uͥngͣtͫips≫ 🥶🥶🥶" * 900000)
